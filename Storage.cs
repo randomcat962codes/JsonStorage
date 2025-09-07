7ea21c7c5611793c5da7ba1410d01b44cab59260
randomcat962codes <landonp1287913+github@gmail.com>
2025-09-07 02:41:09 +0000

    This should be the current version of the package.

---
+++ Storage.cs
@@ -0,0 +1,66 @@
+﻿namespace JsonStorage;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
+
+public class Storage
+{
+    public Dictionary<string, dynamic?> storageObject = new Dictionary<string, dynamic?>();
+    private string file;
+
+    public Storage(string filePath)
+    {
+        file = filePath;
+    }
+
+    public void Check()
+    {
+        if (!File.Exists(file))
+        {
+            File.WriteAllText(file, "{}");
+        }
+    }
+    public void Load()
+    {
+        Check();
+        Dictionary<string, dynamic?>? loadObject = JsonSerializer.Deserialize<Dictionary<string, dynamic?>>(File.ReadAllText(file));
+        if (loadObject != null)
+        {
+            storageObject = loadObject;
+        }
+    }
+    public void Save()
+    {
+        Check();
+        string stringObject = JsonSerializer.Serialize(storageObject, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(file, stringObject);
+    }
+
+    public void QuickDefine(string[] keys, dynamic?[] values)
+    {
+        for (int i = 0; i < keys.Length; i++)
+        {
+            storageObject[keys[i]] = values[i];
+        }
+    }
+
+    public string[] GetKeys()
+    {
+        return storageObject.Keys.ToArray();
+    }
+
+    public void Clear(bool save = true)
+    {
+        
+        string[] keys = GetKeys();
+        for (int i = 0; i < keys.Length; i++)
+        {
+            storageObject.Remove(keys[i]);
+        }
+        if (save)
+        {
+            Save();
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+using JsonStorage;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Storage Storage = new Storage("storage.json");
+        Storage.Load();
+
+        Storage.QuickDefine(["Animal", "Color", "Diet", "Size"], ["Cow", "White/Black/Brown", "Plant", "Large"]);
+        Storage.Save();
+
+        Console.WriteLine("Hello World!");
+    }
+}
